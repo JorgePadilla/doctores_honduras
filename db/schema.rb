77f6741035dc7d069557216477e605af06000f85
JorgePadilla <jorgep4dill4@gmail.com>
2025-11-10 23:26:52 -0500

    Add admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_11_013217) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_11_042406) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -87,7 +87,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_11_013217) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "medical_license"
-    t.string "city"
     t.string "state"
     t.string "subspecialty"
     t.boolean "hidden"
